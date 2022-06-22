5119ff5b3a6d863c6b882efed5e66566c0501ba6
Timofey Chirtsov <teamheightline@mail.ru>
2022-06-22 18:48:22 +0300

    SW-Next - при нажатии на любой элемент навбара пользователь попадет на полную версию SW

---
+++ elements/Navbar/navbar.tsx
@@ -0,0 +1,61 @@
+import {AppBar, Button, Paper, Stack, Toolbar, Typography} from "@mui/material";
+import {PaperProps} from "@mui/material/Paper/Paper";
+import {useRouter} from "next/router";
+
+interface INavbarProps extends PaperProps {
+
+}
+
+
+export default function Navbar({...props}: INavbarProps) {
+    const router = useRouter()
+    return (
+        <Paper elevation={0} sx={{mb: 4}}>
+            <AppBar position="fixed" enableColorOnDark>
+                <Toolbar variant="dense">
+                    <Stack direction={"row"} justifyContent={"space-between"} sx={{width: "100%"}}>
+                        <Typography variant="h6" sx={{color: "white"}}
+                                    onClick={() => {
+                                        router.push('https://sw-university.com')
+                                    }}>
+                            Study Ways Mini
+                        </Typography>
+                        <Stack direction={"row"} alignItems={"center"} justifyContent={"end"}>
+                            <Typography
+                                onClick={() => {
+                                    router.push("https://sw-university.com/courses")
+                                }}
+                                sx={{
+                                    color: "#ffffff", textDecorationColor: "#2D3A4A",
+                                    marginLeft: 25,
+                                    textDecoration: "none"
+                                }}>
+                                Курсы
+                            </Typography>
+
+                            <Typography
+                                onClick={() => {
+                                    router.push("https://sw-university.com/cards")
+                                }}
+                                sx={{
+                                    color: "#ffffff", textDecorationColor: "#2D3A4A", ml: 2,
+                                    textDecoration: "none"
+                                }}>
+                                Карточки
+                            </Typography>
+
+                            <Typography sx={{color: "white", pl: 2,}}> | </Typography>
+
+                            <Button sx={{ml: 2, pr: 2}} color="inherit" variant="outlined"
+                                    onClick={() => {
+                                        router.push("https://sw-university.com/login")
+                                    }}>
+                                Войти
+                            </Button>
+                        </Stack>
+                    </Stack>
+                </Toolbar>
+            </AppBar>
+        </Paper>
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {ThemeProvider} from "@mui/material";
 import ThemeStoreObject from "../global-theme";
+import Navbar from "../elements/Navbar/navbar";
 
 function MyApp({Component, pageProps}: AppProps) {
     return (
         <ThemeProvider theme={ThemeStoreObject.theme}>
-            <Component {...pageProps} />
+            <Navbar/>
+            <Component {...pageProps}/>
         </ThemeProvider>
     )
 }
