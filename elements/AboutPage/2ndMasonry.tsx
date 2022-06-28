350253f88291ee2de98002c8252ec68e15a3b0bc
Timofey Chirtsov <teamheightline@mail.ru>
2022-06-28 17:58:01 +0300

    Оптимизация изображений

--- elements/AboutPage/2ndMasonry.tsx
+++ elements/AboutPage/2ndMasonry.tsx
@@ -3,8 +3,8 @@ import {PaperProps} from "@mui/material/Paper/Paper";
 import * as I2_1 from "../img/2_1.png";
 import * as I2_2 from "../img/2_2.png";
 import * as I2_3 from "../img/2_3.png";
-import * as I2_4 from "../img/2_4.jpeg";
-import * as I2_5 from "../img/2_5.jpeg";
+import * as I2_4 from "../img/2_4.png";
+import * as I2_5 from "../img/2_5.png";
 import {Masonry} from "@mui/lab";
 import Image from "next/image";
 
