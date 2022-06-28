97c8e79c58cb33609ccb2dae397088fe7e0f27d8
Timofey Chirtsov <teamheightline@mail.ru>
2022-06-29 00:14:13 +0300

    Добавил мета теги для страницы о проекте

--- elements/AboutPage/about-page.tsx
+++ elements/AboutPage/about-page.tsx
@@ -2,6 +2,7 @@ import {Grid, Paper, Typography} from "@mui/material";
 import {PaperProps} from "@mui/material/Paper/Paper";
 import Story from "./story";
 import Author from "./author";
+import Head from 'next/head'
 
 interface IAboutPageProps extends PaperProps {
 
@@ -10,6 +11,16 @@ interface IAboutPageProps extends PaperProps {
 export default function AboutPage({...props}: IAboutPageProps) {
     return (
         <Paper elevation={0} {...props}>
+            <Head>
+                <title>Study Ways</title>
+                <meta name="description"
+                      content={" Проект кафедры физики ЛЭТИ.\n" +
+                          "Нашей целью является создание образовательной среды, которая дополнит очное обучение и\n" +
+                          "позволит\n" +
+                          "студентам и преподавателям проще взаимодействовать. Мы хотим предоставить всем студентам\n" +
+                          "равные возможности по получению информации и оценки знаний. https://sw-university.com/"
+                      } key="desc"/>
+            </Head>
             <Grid container justifyContent={"center"}>
                 <Grid item xs={12} md={4}>
                     <Typography variant="h2" textAlign={"center"} sx={{color: "primary.main", mt: 8}}>
