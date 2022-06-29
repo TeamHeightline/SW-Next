30d509be48e75594335d9ea707d06b4b42fbbfa5
Timofey Chirtsov <teamheightline@mail.ru>
2022-06-29 12:45:36 +0300

    Для навбава теперь ссылки сделаны через кнопки с тегом <a/> и атрибутом href

--- elements/Navbar/navbar.tsx
+++ elements/Navbar/navbar.tsx
@@ -1,79 +1,60 @@
 import {AppBar, Button, Paper, Stack, Toolbar, Typography} from "@mui/material";
-import {PaperProps} from "@mui/material/Paper/Paper";
-import {useRouter} from "next/router";
 import Box from '@mui/material/Box';
 import Head from 'next/head'
-
-interface INavbarProps extends PaperProps {
-
-}
+import Link from "next/link";
 
 
-export default function Navbar({...props}: INavbarProps) {
-    const router = useRouter()
+export default function Navbar() {
     return (
         <Paper elevation={0} sx={{mb: 4}}>
             <div>
                 <Head>
                     <title>SW Mini</title>
+                    <meta name="theme-color" content="#0A1929"/>
                 </Head>
             </div>
             <Box sx={{display: {xs: 'none', sm: 'block'}}}>
                 <AppBar position="fixed" enableColorOnDark>
                     <Toolbar variant="dense">
                         <Stack direction={"row"} justifyContent={"space-between"} sx={{width: "100%"}}>
-                            <Typography variant="h6" sx={{color: "white"}}
-                                        onClick={() => {
-                                            router.push('https://sw-university.com')
-                                        }}>
-                                Study Ways Mini
-                            </Typography>
+                            <Link href={"https://sw-university.com"} passHref>
+                                <Button variant="text" sx={{color: "white", fontSize: '1rem'}}>
+                                    Study Ways Mini
+                                </Button>
+                            </Link>
                             <Stack direction={"row"} alignItems={"center"} justifyContent={"end"}>
-                                <Typography
-                                    onClick={() => {
-                                        router.push("https://sw-university.com/courses")
-                                    }}
-                                    sx={{
-                                        color: "#ffffff", textDecorationColor: "#2D3A4A",
-                                        marginLeft: 25,
-                                        textDecoration: "none"
-                                    }}>
-                                    Курсы
-                                </Typography>
+                                <Link href={"https://sw-university.com/courses"} passHref>
+                                    <Button variant={"text"}
+                                            sx={{color: "#ffffff", ml: 2}}>
+                                        Курсы
+                                    </Button>
+                                </Link>
 
-                                <Typography
-                                    onClick={() => {
-                                        router.push("https://sw-university.com/cards")
-                                    }}
-                                    sx={{
-                                        color: "#ffffff", textDecorationColor: "#2D3A4A", ml: 2,
-                                        textDecoration: "none"
-                                    }}>
-                                    Карточки
-                                </Typography>
+                                <Link href={"https://sw-university.com/cards"} passHref>
+                                    <Button variant={"text"}
+                                            sx={{color: "#ffffff", ml: 2}}>
+                                        Карточки
+                                    </Button>
+                                </Link>
 
                                 <Typography sx={{color: "white", ml: 2, mr: 2}}> | </Typography>
 
-                                <Button sx={{ml: 2, pr: 2}} color="inherit" variant="outlined"
-                                        onClick={() => {
-                                            router.push("https://sw-university.com/login")
-                                        }}>
-                                    Войти
-                                </Button>
+                                <Link href={"https://sw-university.com/login"} passHref>
+                                    <Button sx={{ml: 2, pr: 2}} color="inherit" variant="outlined">
+                                        Войти
+                                    </Button>
+                                </Link>
                             </Stack>
                         </Stack>
                     </Toolbar>
                 </AppBar>
             </Box>
             <Box sx={{display: {xs: 'block', sm: 'none'}, width: "100%"}}>
-                <Button color={"primary"}
-                        variant={"contained"}
-                        fullWidth
-                        onClick={() => {
-                            router.push("https://sw-university.com/" + router.asPath)
-                        }}>
-                    Перейти в полную версию SW
-                </Button>
+                <Link href={"https://sw-university.com/"} passHref>
+                    <Button color={"primary"} variant={"contained"} fullWidth>
+                        Перейти в полную версию SW
+                    </Button>
+                </Link>
             </Box>
         </Paper>
     )
